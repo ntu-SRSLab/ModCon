e10df2a152d725555c79542aa1dbd06f011d9424
liuye <liuye5@live.com>
2020-11-03 13:45:11 +0800

    save website

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,4 +4,4 @@ workdir=$(pwd)
 cd $workdir/server && npm install && (node server.js &)
 sleep 120
 # start front-end (client) of ModCon
-cd $workdir/app && npm install && npm run serve
+cd $workdir/app && npm install && npm run serve src/main.js http://155.69.202.66/modcon_server
