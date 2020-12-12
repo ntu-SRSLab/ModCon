f825f0adb747fdef327a77e84b4174a7813c6442
liuye <liuye5@live.com>
2020-12-12 19:33:27 +0800

    save

--- Dockerfile
+++ Dockerfile
@@ -14,6 +14,8 @@ COPY bootstrap.sh /home/modcon/
 RUN apt update && apt install -y git && apt install -y curl
 RUN apt install -y wget nodejs npm &&\
     npm i -g n && n latest && \
-    npm i -g npm@6.14.4 
+    npm i -g npm@6.14.4  
+RUN cd /home/modcon/server && npm install &&\
+    cd /home/modcon/app && npm install 
 
 CMD ["bash", "bootstrap.sh"]
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 # start back-end (server) of ModCon
-
-webserver=$1
-echo $webserver
 workdir=$(pwd)
-cd $workdir/server && npm install && (node server.js &)
+cd $workdir/server && (node server.js &)
 sleep 120
 # start front-end (client) of ModCon
-cd $workdir/app && npm install && npm run serve src/main.js $webserver
+cd $workdir/app && npm run serve
